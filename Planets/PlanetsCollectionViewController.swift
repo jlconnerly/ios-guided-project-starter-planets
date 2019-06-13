2d999bbe3499c374f1ea71809faf5d5e22efac9e
Jake Connerly <jlconnerly@gmail.com>
2019-06-13 12:26:56 -0500

    Added Settings

---
+++ Planets/PlanetsCollectionViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PlanetsCollectionViewController.swift
+//  Planets
+//
+//  Created by Jake Connerly on 6/13/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "PlanetCell"
+
+class PlanetsCollectionViewController: UICollectionViewController {
+
+    let planetController = PlanetController()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView?.reloadData()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return planetController.planets.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PlanetCollectionViewCell
+    
+        let planet = planetController.planets[indexPath.row]
+        cell.imageView.image = planet.image
+        cell.nameLabel.text = planet.name
+    
+        return cell
+    }
+
+    
+    
+}
---
+++ Planets/Views/PlanetCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Jake Connerly on 6/13/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
